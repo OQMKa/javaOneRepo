429beeac3af6f7908d586cb40a864f3b3c7e33c0
OQMKa <omkarnake16@gmail.com>
2025-10-07 16:35:42 +0530

    learned methods - submit() of 3 types [runnable, callable, runnable-task], shutdown()- closes the thread, isShutdown(), awaitTermination()

--- javaOne.java/ClassOne.java
+++ javaOne.java/ClassOne.java
@@ -1,5 +1,28 @@
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 class ClassOne{
     public static void main(String[] a){
-        System.out.println("and from now on i will prioritize myself first always.");
+        
+        ExecutorService executor = Executors.newFixedThreadPool(3);
+
+       // System.out.println("and from now on i will prioritize myself first always.");
+       for (int i =1;i<=10;i++){
+        int finalI = i;
+        System.out.println(fact(i));
+        executor.submit(()->{
+            int result = fact(finalI);
+            System.out.println(result);
+        });
+    }
+    long endTime  = System.currentTimeMillis();
+    System.out.println("endtime : " + endTime);
+    }
+    
+    public static int fact(int n){
+        if (n == 0) return 1;
+        // Thread.sleep(1000);
+        return n*fact(n-1);
+
     }
 }
